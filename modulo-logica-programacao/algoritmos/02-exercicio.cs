c68fbef841765118f855368f066231d0dd53780c
chrisestagio <chrisestagio06@gmail.com>
2025-10-17 17:44:04 -0300

    Add files via upload

---
+++ modulo-logica-programacao/algoritmos/02-exercicio.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+namespace ConsoleApp2
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+          //char emoji = '😊';
+            char emoji = '☺';
+            char letra = 'A';
+            int valor = letra;
+
+            Console.WriteLine("O valor da letra '{letra}' ");
+        }
+    }
+}
